2b324d5fb849a5f41a9d114f54034ba4f0522561
Ryan Hope <rmh3093@gmail.com>
2019-02-21 13:18:14 -0500

    fix typeo

--- eculib/base.py
+++ eculib/base.py
@@ -6,7 +6,7 @@ class KlineAdapter(Device):
 
 	def __init__(self, device_id, baudrate=10400):
 		super(KlineAdapter, self).__init__(device_id, auto_detach=(platform.system()!="Windows"))
-		self.baudraute = baudrate
+		self.baudrate = baudrate
 		self.ftdi_fn.ftdi_usb_reset()
 		self.ftdi_fn.ftdi_set_line_property(8, 1, 0)
 		self.ftdi_fn.ftdi_usb_purge_buffers()
@@ -15,3 +15,5 @@ class KlineAdapter(Device):
 		self.flush()
 		self._write(b"\xff")
 		return self._read(1) == b"\xff"
+
+class ECU(object):
